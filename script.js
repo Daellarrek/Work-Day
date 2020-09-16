0caba5b317e149d232caa80c403c6d5e8e8696ed
David Moler <tigermoler@gmail.com>
2020-09-15 21:56:16 -0700

    Linked JS to HTML. Removed extra time frames. Added favicon. Made time blocks change color.

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
 function simpleCalendar() {
     $(document).ready(function () {
-        let momentVar = moment().format('dddd, MMMM Do, YYYY h:mm a');
-        let workHours = ["9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.",
+        let currentDay = moment().format('dddd, MMMM Do, YYYY h:mm a');
+        let currentHour = ["9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.",
             "5 p.m."]
 
-        //THEN the current day is displayed at the top of the calendar
-        function currentDay() {
-            let currentDay = $("#currentDay").text(momentVar);
-            return currentDay;
-        }
+        //function currentDay() {
+           // let currentDay = $("#currentDay").text(currentDay);
+            //return currentDay;
+        //}
 
-        currentDay();
-        let row;
-        let timeCol;
+        //currentDay();
 
-        //THEN I am presented with timeblocks for standard business hours
-        for (i = 0; i < workHours.length; i++) {
+        for (i = 0; i < currentHour.length; i++) {
             let row = $("<div>").addClass("row");
-            let timeCol = $("<text-area>").text(workHours[i]).addClass("hour");
+            let timeCol = $("<text-area>").text(currentHour[i]).addClass("hour");
             let inputCol = $("<input>").attr("placeholder", "Enter note here").addClass("toDo-input time-block");
             saveBtn = $("<button>").addClass("btn btn-primary saveBtn").text("save");
             $(row).append(timeCol).append(inputCol).append(saveBtn);
             $("#calendar").append(row);
         }
 
-        //for loop to add id's to input fields
         function addIdToInput() {
             let inputId = document.getElementsByClassName("toDo-input");
             let length = inputId.length;
@@ -34,7 +29,6 @@ function simpleCalendar() {
         };
         addIdToInput();
 
-        //for loop to add id's to save buttons
         function addIdToSaveBtn() {
             let saveBtnId = document.getElementsByClassName("saveBtn");
             let length = saveBtnId.length;
@@ -54,96 +48,90 @@ function simpleCalendar() {
         let saveBtnId8 = $("#saveBtn-Id-8");
         let saveBtnId9 = $("#saveBtn-Id-9");
 
-        let toDoInput1 = $("#input-field-1");
-        let toDoInput2 = $("#input-field-2");
-        let toDoInput3 = $("#input-field-3");
-        let toDoInput4 = $("#input-field-4");
-        let toDoInput5 = $("#input-field-5");
-        let toDoInput6 = $("#input-field-6");
-        let toDoInput7 = $("#input-field-7");
-        let toDoInput8 = $("#input-field-8");
-        let toDoInput9 = $("#input-field-9");
-
-
-        let storedInput;
-
-        //click events for each save button 
-
+        let doList1 = $("#input-field-1");
+        let doList2 = $("#input-field-2");
+        let doList3 = $("#input-field-3");
+        let doList4 = $("#input-field-4");
+        let doList5 = $("#input-field-5");
+        let doList6 = $("#input-field-6");
+        let doList7 = $("#input-field-7");
+        let doList8 = $("#input-field-8");
+        let doList9 = $("#input-field-9");
+        
         $(saveBtnId1).on("click", function (event) {
             event.preventDefault();
-            let toDoInput1 = $("#input-field-1").val().trim();
-            localStorage.setItem("toDo1", (toDoInput1));
+            let doList1 = $("#input-field-1").val().trim();
+            localStorage.setItem("toDo1", (doList1));
         });
-        let savedInput1 = (localStorage.getItem("toDo1"));
-        toDoInput1.val(savedInput1);
+        let userData1 = (localStorage.getItem("toDo1"));
+        doList1.val(userData1);
 
         $(saveBtnId2).on("click", function (event) {
             event.preventDefault();
-            let toDoInput2 = $("#input-field-2").val().trim();
-            localStorage.setItem("toDo2", (toDoInput2));
+            let doList2 = $("#input-field-2").val().trim();
+            localStorage.setItem("toDo2", (doList2));
         });
-        let savedInput2 = (localStorage.getItem("toDo2"));
-        toDoInput2.val(savedInput2);
+        let userData2 = (localStorage.getItem("toDo2"));
+        doList2.val(userData2);
 
         $(saveBtnId3).on("click", function (event) {
             event.preventDefault();
-            let toDoInput3 = $("#input-field-3").val().trim();
-            localStorage.setItem("toDo3", (toDoInput3));
+            let doList3 = $("#input-field-3").val().trim();
+            localStorage.setItem("toDo3", (doList3));
         });
-        let savedInput3 = (localStorage.getItem("toDo3"));
-        toDoInput3.val(savedInput3);
+        let userData3 = (localStorage.getItem("toDo3"));
+        doList3.val(userData3);
 
         $(saveBtnId4).on("click", function (event) {
             event.preventDefault();
-            let toDoInput4 = $("#input-field-4").val().trim();
-            localStorage.setItem("toDo4", (toDoInput4));
+            let doList4 = $("#input-field-4").val().trim();
+            localStorage.setItem("toDo4", (doList4));
         });
-        let savedInput4 = (localStorage.getItem("toDo4"));
-        toDoInput4.val(savedInput4);
+        let userData4 = (localStorage.getItem("toDo4"));
+        doList4.val(userData4);
 
         $(saveBtnId5).on("click", function (event) {
             event.preventDefault();
-            let toDoInput5 = $("#input-field-5").val().trim();
-            localStorage.setItem("toDo5", (toDoInput5));
+            let doList5 = $("#input-field-5").val().trim();
+            localStorage.setItem("toDo5", (doList5));
         });
-        let savedInput5 = (localStorage.getItem("toDo5"));
-        toDoInput5.val(savedInput5);
+        let userData5 = (localStorage.getItem("toDo5"));
+        doList5.val(userData5);
 
         $(saveBtnId6).on("click", function (event) {
             event.preventDefault();
-            let toDoInput6 = $("#input-field-6").val().trim();
-            localStorage.setItem("toDo6", (toDoInput6));
+            let doList6 = $("#input-field-6").val().trim();
+            localStorage.setItem("toDo6", (doList6));
         });
-        let savedInput6 = (localStorage.getItem("toDo6"));
-        toDoInput6.val(savedInput6);
+        let userData6 = (localStorage.getItem("toDo6"));
+        doList6.val(userData6);
 
         $(saveBtnId7).on("click", function (event) {
             event.preventDefault();
-            let toDoInput7 = $("#input-field-7").val().trim();
-            localStorage.setItem("toDo7", (toDoInput7));
+            let doList7 = $("#input-field-7").val().trim();
+            localStorage.setItem("toDo7", (doList7));
         });
-        let savedInput7 = (localStorage.getItem("toDo7"));
-        toDoInput7.val(savedInput7);
+        let userData7 = (localStorage.getItem("toDo7"));
+        doList7.val(userData7);
 
         $(saveBtnId8).on("click", function (event) {
             event.preventDefault();
-            let toDoInput8 = $("#input-field-8").val().trim();
-            localStorage.setItem("toDo8", (toDoInput8));
+            let doList8 = $("#input-field-8").val().trim();
+            localStorage.setItem("toDo8", (doList8));
         });
-        let savedInput8 = (localStorage.getItem("toDo8"));
-        toDoInput8.val(savedInput8);
+        let userData8 = (localStorage.getItem("toDo8"));
+        doList8.val(userData8);
 
         $(saveBtnId9).on("click", function (event) {
             event.preventDefault();
-            let toDoInput9 = $("#input-field-8").val().trim();
-            localStorage.setItem("toDo9", (toDoInput9));
+            let doList9 = $("#input-field-8").val().trim();
+            localStorage.setItem("toDo9", (doList9));
         });
-        let savedInput9 = (localStorage.getItem("toDo9"));
-        toDoInput9.val(savedInput9);
-
-        // start if else statements to show past, present, future events
+        let userData9 = (localStorage.getItem("toDo9"));
+        doList9.val(userData9);
 
         let now = new Date().getHours();
+        console.log(now)
         if (now > 8) {
             $("#input-field-0").addClass("past");
         } else if (now >= 8 && now < 9) {
